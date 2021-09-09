eb9407b25bc2110c1f4f7ba098102997ff0894c1
TylerMair <tyleramair@gmail.com>
2021-09-08 20:58:40 -0700

    psuedo code, assigned group functions, adjusted html

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Search bar function
+
+
+//Fetch Flight Price API (Adam and Hayden)
+
+
+//Fetch Covid Cases API (Adam and Hayden)
+
+
+//Save Previous searches to local storage (Tyler)
+
+
+//Function to display searches to screen (Tyler)
